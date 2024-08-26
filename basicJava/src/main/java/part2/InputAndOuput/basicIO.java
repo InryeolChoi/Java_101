94d2908627bf71c443783e016cfd43de7727eb49
InryeolChoi <dlsfuf0316@gmail.com>
2024-08-26 15:45:56 +0900

    조건문 추가

--- basicJava/src/main/java/part2/InputAndOuput/basicIO.java
+++ basicJava/src/main/java/part2/InputAndOuput/basicIO.java
@@ -4,16 +4,12 @@ import java.util.Scanner;
 
 // 기본적인 입출력
 public class basicIO {
-    public static void main(String[] args) throws IOException {
-        try {
-            Scanner sc = new Scanner(System.in);
-            String s1 = sc.next();
-            System.out.println(s1);
-        }
-        finally {
-            // 마지막에 파일 디스크립터를 닫아주는 것이 필요
-            sc.close();
-        }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s1 = sc.next();
+        System.out.println(s1);
 
+        // 마지막에 파일 디스크립터를 닫아주는 것이 필요
+        sc.close();
     }
 }
--- basicJava/src/main/java/part3/basicGrammar/Condition.java
+++ basicJava/src/main/java/part3/basicGrammar/Condition.java
@@ -1,9 +1,54 @@
 package part3.basicGrammar;
 
+import java.io.*;
+
 public class Condition {
-    public static void main(String[] args) {
+    private BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    private BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    public static void main(String[] args) throws IOException {
         Condition condition = new Condition();
+        int number;
+
+        try {
+            number = Integer.parseInt(condition.br.readLine()); // static으로 써야 함.
+            System.out.println(number);
+        } catch (NumberFormatException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        try {
+            condition.thinkIf(number);
+            condition.thinkSwitch(number);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } finally {
+            condition.br.close();
+            condition.bw.close();
+        }
     }
 
+    public void thinkIf(int x) throws IOException {
+        if (x != 5)
+            bw.write("5이다");
+        else
+            bw.write("5가 아니다");
+    }
+
+    public void thinkSwitch(int x) throws IOException {
+        switch(x) {
+            case 5:
+                bw.write("5이다.");
+                break;
+            case 6:
+                bw.write("6이다");
+                break;
+            default:
+                bw.write("지정되지 않은 숫자입니다.\n");
+                break;
+        }
+    }
 
 }
